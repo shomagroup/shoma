56a0d64f9cfc3e93f46ed7e78522d8ed422592fe
shomagroup <kcasana@shomagroup.com>
2022-11-11 16:14:22 -0500

    Update main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+// INITIAL SETTINGS
+
+
+// CUSTOM MOUSE
+(function() {
+    var cursor = document.querySelector(".cursor");
+    var cursorLazy = document.querySelector(".cursor-lazy");
+    var cInfo = document.querySelector(".c-info");
+    var cursorActive = function cursorActive(event) {
+        cursor.style.left = event.clientX + "px";
+        cursor.style.top = event.clientY + "px";
+        cursorLazy.style.left = event.clientX + "px";
+        cursorLazy.style.top = event.clientY + "px";
+        cInfo.style.left = event.clientX + "px";
+        cInfo.style.top = event.clientY + "px";
+    };
+    window.addEventListener("mousemove", cursorActive);
+})();
