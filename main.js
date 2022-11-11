d4950d335381709f2b0f6b7dfdd20e9d76f5b146
shomagroup <kcasana@shomagroup.com>
2022-11-11 16:27:26 -0500

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,40 @@
     };
     window.addEventListener("mousemove", cursorActive);
 })();
+// INIT SET
+
+// CURSOR HOVER
+$('[white]').on('mouseenter mouseleave', function() {
+    $('.cursor').toggleClass('white');
+});
+$('a').on('mouseenter mouseleave', function() {
+    $('.c-info').text($(this).attr('info'));
+    if ($(this).attr('info') && $(this).attr('white')) {
+        $('.cursor').toggleClass('active white');
+        $('.c-info').toggleClass('active');
+    } else if ($(this).attr('info')) {
+        $('.cursor').toggleClass('active');
+        $('.c-info').toggleClass('active');
+    } else if ($(this).attr('white')) {
+        $('.cursor').toggleClass('active white');
+    } else {
+        $('.cursor').toggleClass('active');
+    }
+});
+
+// MENU
+$('.m-button').on('click', function() {
+    $('.menu').toggleClass('opened');
+    if ($(this).is('.active')) {
+        $(this).toggleClass('active');
+        $(this).attr('info', 'menu');
+        $('.c-info').text('menu');
+    } else {
+        $(this).toggleClass('active');
+        $(this).attr('info', 'close');
+        $('.c-info').text('close');
+    }
+});
+$('.m-link').on('mouseenter mouseleave', function() {
+    $(this).siblings().toggleClass('nothovered');
+});
