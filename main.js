d700389e4a38e71c93663bbca19b6e1f78dca226
shomagroup <kcasana@shomagroup.com>
2022-11-16 11:21:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,71 @@
     };
     window.addEventListener("mousemove", cursorActive);
 })();
+// BETA CHECK
+$('.update-check').text('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+$('.update-check').addClass('show');
+setTimeout(function() {
+    $('.update-check').removeClass('show');
+}, 1500);
+
+// ClASS CLENSER
+$('.active').removeClass('active');
+// INIT SET
+
+// LANGUAGE SETTING 
+$('.language[en]').addClass('active');
+
+// ------ CURSOR HOVER -----
+// WHITE
+$('[white]').on('mouseover', function() {
+    $('.cursor').addClass('white');
+});
+$('[white]').on('mouseleave', function() {
+    $('.cursor').removeClass('white');
+});
+// LINK HOVER
+$('a').on('mouseenter', function() {
+    $('.c-info').text($(this).attr('info'));
+    if ($(this).attr('info') && $(this).attr('white')) {
+        $('.cursor').addClass('active white');
+        $('.c-info').addClass('active');
+    } else if ($(this).attr('info')) {
+        $('.cursor').addClass('active');
+        $('.c-info').addClass('active');
+    } else if ($(this).attr('white')) {
+        $('.cursor').addClass('active white');
+    } else {
+        $('.cursor').addClass('active');
+    }
+});
+$('a').on('mouseleave', function() {
+    $('.c-info').text($(this).attr('info'));
+    if ($(this).attr('info') && $(this).attr('white')) {
+        $('.cursor').removeClass('active white');
+        $('.c-info').removeClass('active');
+    } else if ($(this).attr('info')) {
+        $('.cursor').removeClass('active');
+        $('.c-info').removeClass('active');
+    } else if ($(this).attr('white')) {
+        $('.cursor').removeClass('active white');
+    } else {
+        $('.cursor').removeClass('active');
+    }
+});
+
+// MENU
+$('.m-button').on('click', function() {
+    $('.menu').toggleClass('active');
+    if ($(this).is('.active')) {
+        $(this).toggleClass('active');
+        $(this).attr('info', 'menu');
+        $('.c-info').text('menu');
+    } else {
+        $(this).toggleClass('active');
+        $(this).attr('info', 'close');
+        $('.c-info').text('close');
+    }
+});
+$('.m-link').on('mouseenter mouseleave', function() {
+    $(this).siblings().toggleClass('nothovered');
+});
