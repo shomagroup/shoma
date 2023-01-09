ff8a65504b09021de434504adede27431995def7
shomagroup <kcasana@shomagroup.com>
2023-01-09 16:53:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
 })();
 
 // BETA CHECK
-$('.update-check').text('draggyBeta8.2');
+$('.update-check').text('draggyBeta8.3');
 $('.update-check').addClass('show');
 setTimeout(function() {
     $('.update-check').removeClass('show');
@@ -48,7 +48,7 @@ $('a').on('mouseover', function() {
     $('.c-info').text($(this).attr('info'));
     if ($(this).attr('info') && $(this).attr('white')) {
         $('.cursor').addClass('active white');
-
+        $('.c-info').addClass('active');
     } else if ($(this).attr('info')) {
         $('.cursor').addClass('active');
         $('.c-info').addClass('active');
@@ -57,8 +57,8 @@ $('a').on('mouseover', function() {
     } else {
         $('.cursor').addClass('active');
     }
-    if ($(this).attr('draggy') && $(this).attr('info')) {
-        $('.c-info').text('aaa');
+    if ($(this).attr('draggy')) {
+        $('.c-info').text('VIEW');
     }
 });
 $('a').on('mouseout', function() {
