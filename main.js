3dd79ce7c5fb7cce6a3be7029f9d8dcff8fe1917
shomagroup <kcasana@shomagroup.com>
2023-01-20 14:55:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
 })();
 
 // BETA CHECK
-$('.update-check').text('slider 1.2');
+$('.update-check').text('slider 1.3');
 $('.update-check').addClass('show');
 setTimeout(function() {
     $('.update-check').removeClass('show');
