a16512633c1d7cd77760a7d2b69cca3773168d29
shomagroup <kcasana@shomagroup.com>
2023-01-20 14:35:39 -0500

    Update slider-1.js

--- homepage/slider-1.js
+++ homepage/slider-1.js
@@ -60,3 +60,34 @@ function momentumLoop() {
         momentumID = requestAnimationFrame(momentumLoop);
     }
 }
+
+//// another slider please
+
+const slider2 = document.querySelector('.press-container');
+let isDown2 = false;
+let startX2;
+let scrollLeft2;
+
+slider2.addEventListener('mousedown', (e) => {
+    isDown2 = true;
+    slider2.classList.add('active');
+    startX2 = e.pageX - slider2.offsetLeft;
+    scrollLeft2 = slider2.scrollLeft;
+});
+slider2.addEventListener('mouseleave', () => {
+    isDown2 = false;
+    slider2.classList.remove('active');
+
+});
+slider2.addEventListener('mouseup', () => {
+    isDown2 = false;
+    slider2.classList.remove('active');
+
+});
+slider2.addEventListener('mousemove', (e) => {
+    if (!isDown) return; //stop function
+    e.preventDefault();
+    const x2 = e.pageX - slider2.offsetLeft;
+    const walk2 = x2 - startX2
+    console.log(walk2);
+});
