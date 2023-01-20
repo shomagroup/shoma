64c78fce8cfc705c6d49e7f9798c0c96672673f3
shomagroup <kcasana@shomagroup.com>
2023-01-20 12:22:40 -0500

    Update slider-1.js

--- homepage/slider-1.js
+++ homepage/slider-1.js
@@ -1,38 +1,38 @@
-const slider = document.querySelector('.people-container, .press-container');
+const slider1 = document.querySelector('.people-container');
 let isDown = false;
 let startX;
 let scrollLeft;
 
-slider.addEventListener('mousedown', (e) => {
+slider1.addEventListener('mousedown', (e) => {
     isDown = true;
-    slider.classList.add('active');
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
+    slider1.classList.add('active');
+    startX = e.pageX - slider1.offsetLeft;
+    scrollLeft = slider1.scrollLeft;
     cancelMomentumTracking();
 });
 
 
-slider.addEventListener('mouseleave', () => {
+slider1.addEventListener('mouseleave', () => {
     isDown = false;
-    slider.classList.remove('active');
+    slider1.classList.remove('active');
 });
 
 
-slider.addEventListener('mouseup', () => {
+slider1.addEventListener('mouseup', () => {
     isDown = false;
-    slider.classList.remove('active');
+    slider1.classList.remove('active');
     beginMomentumTracking();
 });
 
 
-slider.addEventListener('mousemove', (e) => {
+slider1.addEventListener('mousemove', (e) => {
     if (!isDown) return;
     e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
+    const x = e.pageX - slider1.offsetLeft;
     const walk = (x - startX) * 1.1; //scroll-fast
-    var prevScrollLeft = slider.scrollLeft;
-    slider.scrollLeft = scrollLeft - walk;
-    velX = slider.scrollLeft - prevScrollLeft;
+    var prevScrollLeft = slider1.scrollLeft;
+    slider1.scrollLeft = scrollLeft - walk;
+    velX = slider1.scrollLeft - prevScrollLeft;
 });
 
 // Momentum 
@@ -40,7 +40,7 @@ slider.addEventListener('mousemove', (e) => {
 var velX = 0;
 var momentumID;
 
-slider.addEventListener('wheel', (e) => {
+slider1.addEventListener('wheel', (e) => {
     cancelMomentumTracking();
 });
 
@@ -54,9 +54,74 @@ function cancelMomentumTracking() {
 }
 
 function momentumLoop() {
-    slider.scrollLeft += velX;
+    slider1.scrollLeft += velX;
     velX *= 0.95;
     if (Math.abs(velX) > 0.5) {
         momentumID = requestAnimationFrame(momentumLoop);
+    }
+}
+
+/// Code again
+
+const slider2 = document.querySelector('.press-container');
+let isDown2 = false;
+let startX2;
+let scrollLeft2;
+
+slider2.addEventListener('mousedown', (e) => {
+    isDown2 = true;
+    slider2.classList.add('active');
+    startX2 = e.pageX - slider2.offsetLeft;
+    scrollLeft2 = slider2.scrollLeft2;
+    cancelMomentumTracking();
+});
+
+
+slider1.addEventListener('mouseleave', () => {
+    isDown2 = false;
+    slider2.classList.remove('active');
+});
+
+
+slider2.addEventListener('mouseup', () => {
+    isDown2 = false;
+    slider2.classList.remove('active');
+    beginMomentumTracking();
+});
+
+
+slider2.addEventListener('mousemove', (e) => {
+    if (!isDown2) return;
+    e.preventDefault();
+    const x = e.pageX - slider2.offsetLeft;
+    const walk = (x - startX2) * 1.1; //scroll-fast
+    var prevScrollLeft = slider2.scrollLeft2;
+    slider2.scrollLeft2 = scrollLeft2 - walk;
+    velX = slider2.scrollLeft - prevScrollLeft;
+});
+
+// Momentum 
+
+var velX2 = 0;
+var momentumID2;
+
+slider1.addEventListener('wheel', (e) => {
+    cancelMomentumTracking();
+});
+
+function beginMomentumTracking() {
+    cancelMomentumTracking();
+    momentumID2 = requestAnimationFrame(momentumLoop2);
+}
+
+function cancelMomentumTracking() {
+    cancelAnimationFrame(momentumID2);
+}
+
+function momentumLoop2() {
+    slider1.scrollLeft += velX;
+    velX2 *= 0.95;
+    if (Math.abs(velX2) > 0.5) {
+        momentumID2 = requestAnimationFrame(momentumLoop2);
     }
 }
