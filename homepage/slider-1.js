92962b63656058bdbb575bcf8e9feac19b43a0c9
shomagroup <kcasana@shomagroup.com>
2023-01-20 12:28:58 -0500

    Update slider-1.js

--- homepage/slider-1.js
+++ homepage/slider-1.js
@@ -1,38 +1,38 @@
-const slider1 = document.querySelector('.press-container');
+const slider = document.querySelector('.people-container');
 let isDown = false;
 let startX;
 let scrollLeft;
 
-slider1.addEventListener('mousedown', (e) => {
+slider.addEventListener('mousedown', (e) => {
     isDown = true;
-    slider1.classList.add('active');
-    startX = e.pageX - slider1.offsetLeft;
-    scrollLeft = slider1.scrollLeft;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
     cancelMomentumTracking();
 });
 
 
-slider1.addEventListener('mouseleave', () => {
+slider.addEventListener('mouseleave', () => {
     isDown = false;
-    slider1.classList.remove('active');
+    slider.classList.remove('active');
 });
 
 
-slider1.addEventListener('mouseup', () => {
+slider.addEventListener('mouseup', () => {
     isDown = false;
-    slider1.classList.remove('active');
+    slider.classList.remove('active');
     beginMomentumTracking();
 });
 
 
-slider1.addEventListener('mousemove', (e) => {
+slider.addEventListener('mousemove', (e) => {
     if (!isDown) return;
     e.preventDefault();
-    const x = e.pageX - slider1.offsetLeft;
+    const x = e.pageX - slider.offsetLeft;
     const walk = (x - startX) * 1.1; //scroll-fast
-    var prevScrollLeft = slider1.scrollLeft;
-    slider1.scrollLeft = scrollLeft - walk;
-    velX = slider1.scrollLeft - prevScrollLeft;
+    var prevScrollLeft = slider.scrollLeft;
+    slider.scrollLeft = scrollLeft - walk;
+    velX = slider.scrollLeft - prevScrollLeft;
 });
 
 // Momentum 
@@ -40,7 +40,7 @@ slider1.addEventListener('mousemove', (e) => {
 var velX = 0;
 var momentumID;
 
-slider1.addEventListener('wheel', (e) => {
+slider.addEventListener('wheel', (e) => {
     cancelMomentumTracking();
 });
 
@@ -54,11 +54,9 @@ function cancelMomentumTracking() {
 }
 
 function momentumLoop() {
-    slider1.scrollLeft += velX;
+    slider.scrollLeft += velX;
     velX *= 0.95;
     if (Math.abs(velX) > 0.5) {
         momentumID = requestAnimationFrame(momentumLoop);
     }
 }
-
-/// Code again
