ab1ea6ed701e8f30f7434e44204912c9ec004b20
shomagroup <kcasana@shomagroup.com>
2023-01-20 10:19:52 -0500

    Create slider-1.js

---
+++ homepage/slider-1.js
@@ -0,0 +1,62 @@
+const slider = document.querySelector('.people-container');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+    cancelMomentumTracking();
+});
+
+
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+
+
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+    beginMomentumTracking();
+});
+
+
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1.1; //scroll-fast
+    var prevScrollLeft = slider.scrollLeft;
+    slider.scrollLeft = scrollLeft - walk;
+    velX = slider.scrollLeft - prevScrollLeft;
+});
+
+// Momentum 
+
+var velX = 0;
+var momentumID;
+
+slider.addEventListener('wheel', (e) => {
+    cancelMomentumTracking();
+});
+
+function beginMomentumTracking() {
+    cancelMomentumTracking();
+    momentumID = requestAnimationFrame(momentumLoop);
+}
+
+function cancelMomentumTracking() {
+    cancelAnimationFrame(momentumID);
+}
+
+function momentumLoop() {
+    slider.scrollLeft += velX;
+    velX *= 0.95;
+    if (Math.abs(velX) > 0.5) {
+        momentumID = requestAnimationFrame(momentumLoop);
+    }
+}
