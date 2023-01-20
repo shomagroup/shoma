a0185317fd1446259160a50bc156b9ced06d052e
shomagroup <kcasana@shomagroup.com>
2023-01-20 12:25:48 -0500

    Update slider-1.js

--- homepage/slider-1.js
+++ homepage/slider-1.js
@@ -1,4 +1,4 @@
-const slider1 = document.querySelector('.people-container');
+const slider1 = document.querySelector('.press-container');
 let isDown = false;
 let startX;
 let scrollLeft;
@@ -62,66 +62,3 @@ function momentumLoop() {
 }
 
 /// Code again
-
-const slider2 = document.querySelector('.press-container');
-let isDown2 = false;
-let startX2;
-let scrollLeft2;
-
-slider2.addEventListener('mousedown', (e) => {
-    isDown2 = true;
-    slider2.classList.add('active');
-    startX2 = e.pageX - slider2.offsetLeft;
-    scrollLeft2 = slider2.scrollLeft2;
-    cancelMomentumTracking();
-});
-
-
-slider1.addEventListener('mouseleave', () => {
-    isDown2 = false;
-    slider2.classList.remove('active');
-});
-
-
-slider2.addEventListener('mouseup', () => {
-    isDown2 = false;
-    slider2.classList.remove('active');
-    beginMomentumTracking();
-});
-
-
-slider2.addEventListener('mousemove', (e) => {
-    if (!isDown2) return;
-    e.preventDefault();
-    const x = e.pageX - slider2.offsetLeft;
-    const walk = (x - startX2) * 1.1; //scroll-fast
-    var prevScrollLeft = slider2.scrollLeft2;
-    slider2.scrollLeft2 = scrollLeft2 - walk;
-    velX = slider2.scrollLeft - prevScrollLeft;
-});
-
-// Momentum 
-
-var velX2 = 0;
-var momentumID2;
-
-slider1.addEventListener('wheel', (e) => {
-    cancelMomentumTracking();
-});
-
-function beginMomentumTracking() {
-    cancelMomentumTracking();
-    momentumID2 = requestAnimationFrame(momentumLoop2);
-}
-
-function cancelMomentumTracking() {
-    cancelAnimationFrame(momentumID2);
-}
-
-function momentumLoop2() {
-    slider1.scrollLeft += velX;
-    velX2 *= 0.95;
-    if (Math.abs(velX2) > 0.5) {
-        momentumID2 = requestAnimationFrame(momentumLoop2);
-    }
-}
