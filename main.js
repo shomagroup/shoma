f9b1bc5dea58799dc0da3a6bd9629e1bc2339b36
shomagroup <kcasana@shomagroup.com>
2023-06-02 16:08:05 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 // Update Log
 console.log('ver 2023.06.02 0.0');
+
+
+// CUSTOM MOUSE
+(function() {
+    var cursor = document.querySelector(".cursor");
+    var cursorLazy = document.querySelector(".cursor-lazy");
+    var cInfo = document.querySelector(".c-info");
+    var cursorActive = function cursorActive(event) {
+        cursor.style.left = event.clientX + "px";
+        cursor.style.top = event.clientY + "px";
+        cursorLazy.style.left = event.clientX + "px";
+        cursorLazy.style.top = event.clientY + "px";
+        cInfo.style.left = event.clientX + "px";
+        cInfo.style.top = event.clientY + "px";
+    };
+    window.addEventListener("mousemove", cursorActive);
+})();
