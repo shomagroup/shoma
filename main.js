66bf24866c156dcaca222cc2c7eafe8e0e38246b
shomagroup <92377989+shomagroup@users.noreply.github.com>
2023-07-10 10:33:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ $('.codever').text(codeVer);
 
 // ClASS CLENSER
 $('.active').removeClass('active');
-$('.editing').removeClass('editing');
+$('.c-wrap').removeClass('off');
 
 
 // ------ CURSOR HOVER ----- //
