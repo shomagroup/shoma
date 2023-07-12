5122822a8ac48c87084bfacdecd6d310b0046382
shomagroup <92377989+shomagroup@users.noreply.github.com>
2023-07-12 14:16:37 -0400

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,23 +1,25 @@
 //== [open/close Search]
 $('.search-button a').on('click', function() {
-    if ($('.search').is('.active')) {
-    $('.search').removeClass('active');
-    $('input.search').val('');
-    $(this).attr('info', 'Search')
-    $('.project').removeClass('hidden');
-    } else {
-    $('.search').addClass('active');
-    $(this).attr('info', 'Close')
-    }
-    });
-    //make tex tagger lowercase for filter
-    $('.tagger p').text(function(i, text) {
-    return text.toLowerCase();
-    });
-    //filter for searchbar
-    $('input.search').on('input', function() {
-    var searchValue = $(this).val().toLowerCase();
-    $('.tagger p').closest('.project').hide();
-    $('.tagger p').filter(':contains(' + searchValue + ')')
-    .closest('.project').show();
-    });
+if ($('.search').is('.active')) {
+$('.search').removeClass('active');
+$('input.search').val('');
+$(this).attr('info', 'Search')
+$('.project').removeClass('hidden');
+$('.portfolio-link:nth-child(2)').addClass('filtered');
+} else {
+$('.portfolio-link').removeClass('filtered');
+$('.search').addClass('active');
+$(this).attr('info', 'Close')
+}
+});
+//make tex tagger lowercase for filter
+$('.tagger p').text(function(i, text) {
+return text.toLowerCase();
+});
+//filter for searchbar
+$('input.search').on('input', function() {
+var searchValue = $(this).val().toLowerCase();
+$('.tagger p').closest('.project').hide();
+$('.tagger p').filter(':contains(' + searchValue + ')')
+.closest('.project').show();
+});
